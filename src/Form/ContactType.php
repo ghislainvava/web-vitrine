282c40de5b050821be51ac5b3dd32ec6a44dde95
Nom d'utilisateur générique <votre-email@example.com>
2024-02-08 17:55:34 +0100

    v2

---
+++ src/Form/ContactType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\{TextType, EmailType, TextareaType, SubmitType};
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => 'Nom'])
+            ->add('email', EmailType::class, ['label' => 'Email'])
+            ->add('message', TextareaType::class, ['label' => 'Message'])
+            ->add('send', SubmitType::class, ['label' => 'Envoyer']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([]);
+    }
+}
